bc6fa66ee5dfa90ee7d113fa4a0e4fcdd85922fe
Ben West <bewest@gmail.com>
2014-06-07 18:34:21 -0700

    initial checkin

---
+++ server.js
@@ -0,0 +1,72 @@
+
+
+
+var http = require('http')
+  , ecstatic = require('ecstatic')
+  , ws = require('socket.io')
+  , client = require('socket.io-client')
+  , es = require('event-stream')
+  ;
+
+function monitor (ep, dest) {
+  var source = ep.endpoint;
+  console.log(ep, source);
+  var sock = client.connect(source);
+  sock.on('event', console.log.bind(console, 'EVENT'));
+  sock.on('now', console.log.bind(console, 'now'));
+  sock.on('sgv', function (data) {
+    console.log('ep', ep, 'got data', data.length);
+    // dest.sockets.in(ep.color).emit('sgv', data);
+    dest.sockets.in(ep.color).emit('pool', ep, data);
+  });
+  sock.on('connect', console.log.bind(console, 'connect'));
+  sock.on('error', console.log.bind(console, 'error'));
+  sock.on('disconnect', console.log.bind(console, 'disconnect'));
+  return sock;
+}
+
+function createServer (opts) {
+  var server = http.createServer(
+        ecstatic({
+          root: __dirname + '/static/'
+        , autoIndex: true
+        // , showDir: false
+        })
+      );
+  var io = ws.listen(server);
+  io.sockets.on('connection', function (socket) {
+    var backends = { };
+    console.log("connected", arguments);
+    socket.on('subscribe', function (ep) {
+      // backends.push(ep);
+      backends[ep.endpoint] = monitor(ep, io);
+      // ep.backend =
+      socket.join(ep.color);
+      console.log("subscribed", arguments);
+    });
+    socket.on('unsubscribe', function (ep) {
+      console.log('leaving', arguments, ep.color);
+      if (ep.endpoint in backends) {
+        console.log('backend', backends[ep.endpoint]);
+        backends[ep.endpoint].disconnect( );
+      }
+      // backends.forEach(function (item) { });
+      socket.leave(ep.color);
+    });
+  });
+  // sources.forEach(function (src) { monitor(src, io); });
+  return server;
+
+}
+
+var sources = [
+    'ws://nightscouthd.azurewebsites.net/'
+  , 'ws://localhost:8181/'
+  , 'http://ba-cgm.azurewebsites.net/'
+];
+
+if (!module.parent) {
+  var port = process.env.PORT || 9090;
+  var server = createServer({ }).listen(port);
+  console.log('listening on http://localhost:%s', port);
+}
---
+++ static/js/aggregate.js
@@ -0,0 +1,122 @@
+function toggle_menu (ev) {
+  var settings = $(this).closest('.settings');
+  var view = settings.find('.view');
+  if (view.is('.active')) {
+    view.removeClass('active');
+    view.addClass('hidden');
+    settings.trigger('saved');
+    // save / re-render
+  } else {
+    view.addClass('active');
+    view.removeClass('hidden');
+
+  }
+}
+function config_master ( ) {
+    var socket = io.connect();
+
+    socket.on('now', function (d) {
+        now = d;
+        var dateTime = new Date(now);
+        $('#currentTime').text(d3.time.format('%I:%M%p')(dateTime));
+
+        // Dim the screen by reducing the opacity when at nighttime
+        if (opacity.current != opacity.NIGHT && (dateTime.getHours() > 21 || dateTime.getHours() < 7 )) {
+            $('body').css({'opacity': opacity.NIGHT});
+        } else {
+            $('body').css({'opacity': opacity.DAY});
+        }
+    });
+
+    socket.on('pool', function (ep, data) {
+      var a = data[0];
+      var b = data[1];
+      var c = data[2];
+      var d = data[3];
+      console.log('GOT DATA', ep, a.length, b.length, c.length, d.length);
+
+      $('.endpoints ' + ep.color).trigger('data', data);
+
+    });
+    return socket;
+}
+
+function subscribe (ev) {
+  var ep = $(this).closest('LI.describe');
+  console.log('subscribe this', this);
+  if (true || !ep.is('.ready')) {
+    ep.addClass('ready');
+    var opts = {
+      color: ep.find('.color').text( ) 
+    , endpoint: ep.find('.endpoint').val( )
+    };
+    ep.attr('id', opts.color.slice(1));
+    console.log('subscribing to', opts);
+    my.master.emit('subscribe', opts);
+  }
+  if ($(this).is("FORM")) {
+    console.log('cancel form submit');
+    ev.preventDefault( );
+  }
+
+}
+
+function add_row (ev) {
+  var settings = $(this).closest('.settings');
+  var clone = settings.find('.endpoints .template').clone(true);
+  clone.removeClass('template').addClass('describe');
+  var color = randomColor( );
+  console.log('new', color);
+  clone.find('.color').text(color);
+  clone.find('.color, .chrome').css({color: color});
+  settings.find('.endpoints').append(clone); 
+  clone.find('INPUT.endpoint').focus( );
+}
+
+function remove_row (ev) {
+  var control = $(this).closest('LI');
+  var opts = {
+    color: control.find('.color').text( ) 
+  , endpoint: control.find('.endpoint').val( )
+  };
+  my.master.emit('unsubscribe', opts);
+  control.remove( );
+}
+var my = {endpoints: [ ] };
+
+function update_data (ev, glucose, predict, alarms, treatments) {
+  console.log('UPDATE DATA', 'target', ev.target, 'this', this, $(this).find('.currentBG'));
+  // console.log('args', arguments);
+  // var glucose = data[0];
+  var current = glucose.slice(-1)[0].y;
+  $(ev.target).find('.currentBG').text(current);
+
+  var data = glucose.map(function (obj) {
+    return { date: new Date(obj.x), sgv: obj.y, color: 'grey'}
+  });
+  data = data.concat(predict.map(function (obj) {
+    return { date: new Date(obj.x), sgv: obj.y, color: 'blue'}
+  }));
+  data = data.concat(alarms.map(function (obj) {
+    return { date: new Date(obj.x), sgv: obj.y, color: 'red'}
+  }));
+}
+
+$(document).ready(function ( ) {
+  console.log('aggregator ready', $('.settings, .settings button.toggle'));
+  $('.settings button.toggle').on('click', toggle_menu);
+  $('.settings button.add').on('click', add_row);
+  $('.settings button.nix').on('click', remove_row);
+  // $('.settings button.sync').on('click', subscribe);
+  $('.settings FORM.config').on('submit', subscribe);
+  $('.endpoints').on('data', update_data);
+  my.master = config_master( );
+  console.log('socket', my.master);
+  $(document.body).on('keypress', function (ev) {
+    if (!$(ev.target).is(":input") && ev.which == ('+').charCodeAt(0)) {
+      ev.preventDefault( );
+      $('.settings button.add').trigger('click');
+    }
+
+  });
+});
